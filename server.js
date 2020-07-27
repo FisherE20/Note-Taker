c9b1cca2cdd1347508344d1df666091c71705f82
Erin Fisher <flybirdie85@gmail.com>
2020-07-27 12:13:31 -0700

    Added some server side code

---
+++ server.js
@@ -0,0 +1,28 @@
+//=========================================================
+//Dependencies
+//=========================================================
+let express = require("express");
+//=========================================================
+//Express Config
+//=========================================================
+
+//Tell node that we are creating an "express" server
+let app = express();
+//Sets an inital port. 
+let PORT = 6000;
+console.log("Successfully opened port 6000");
+
+//Sets up the Express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//=========================================================
+//ROUTER
+//=========================================================
+
+
+//=========================================================
+//Listener
+//=========================================================
+app.listen(PORT, function(){
+    console.log("App listening on PORT:" + PORT);
+});
